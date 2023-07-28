f6b9894f0e7dd3138804782cb1b49c5fbcc6f264
King Ho <hosaipang@gmail.com>
2023-07-28 15:27:09 +0800

    Debug on data type.

--- openai_api.py
+++ openai_api.py
@@ -208,4 +208,4 @@ if __name__ == "__main__":
     # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
     model.eval()
 
-    uvicorn.run(app, host='0.0.0.0', port=os.environ.get("PORT", 8000), workers=1)
+    uvicorn.run(app, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)), workers=1)
