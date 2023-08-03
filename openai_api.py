2b7344779d7ce84eca0b706af3ab1a56efd60d95
King Ho <hosaipang@gmail.com>
2023-08-03 14:45:35 +0800

    Revert "Update model name."
    This reverts commit 5df389ad84836083d634056479ffc8467d4c8087.

--- openai_api.py
+++ openai_api.py
@@ -202,8 +202,8 @@ async def create_embeddings(_: Annotated[str, Depends(api_key_header)], text: An
 
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("./model", trust_remote_code=True)
-    model = AutoModel.from_pretrained("./model", trust_remote_code=True).cuda()
+    tokenizer = AutoTokenizer.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True)
+    model = AutoModel.from_pretrained("THUDM/chatglm2-6b", trust_remote_code=True).cuda()
     # 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
     # from utils import load_model_on_gpus
     # model = load_model_on_gpus("THUDM/chatglm2-6b", num_gpus=2)
